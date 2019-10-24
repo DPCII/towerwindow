9989b60e84132e3a6efc22b5d3f00e60af7106a0
D Paul Childs <clausewitzian@gmail.com>
2019-10-23 21:02:01 -0400

    Updates

---
+++ precache-manifest.12105ea57435cf4b38f082ae98be3111.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14bef3546ca8aeb44bf57847498d422a",
+    "url": "/tube-api-test/index.html"
+  },
+  {
+    "revision": "dabeeb88a000e23b1f1b",
+    "url": "/tube-api-test/static/css/main.33b6e2a7.chunk.css"
+  },
+  {
+    "revision": "3f9fd75d21a30bc7fdd8",
+    "url": "/tube-api-test/static/js/2.ed85e5bd.chunk.js"
+  },
+  {
+    "revision": "dabeeb88a000e23b1f1b",
+    "url": "/tube-api-test/static/js/main.3d5bd57d.chunk.js"
+  },
+  {
+    "revision": "dea8de95fe22ecdfbd84",
+    "url": "/tube-api-test/static/js/runtime-main.8a5726e1.js"
+  },
+  {
+    "revision": "18aaeece6546346b855be8fd5017c55b",
+    "url": "/tube-api-test/static/media/window_logo.18aaeece.png"
+  }
+]);
